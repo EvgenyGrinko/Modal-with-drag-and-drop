7ac342b879ead717e18d4b98a42c7fa9c08689c6
EvgenyGrinko <john_forever_young@mail.ru>
2021-02-24 17:56:58 +0300

    Add init files

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <button>Upload</button>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
