84162aa5871b010b29d6c150e4dddefce332f9a3
EvgenyGrinko <john_forever_young@mail.ru>
2021-02-25 20:47:13 +0300

    Refactor Modal styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
 import './Modal.scss';
+import rainSVG from '../../assets/images/rain.svg';
+import cloudsSVG from '../../assets/images/clouds.svg';
 
 interface IModalProps {
     modalVisible: boolean,
@@ -32,25 +34,41 @@ export const Modal = ({ modalVisible, setModalVisible }: IModalProps) => {
     }
 
     const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const {target} = event;
+        const { target } = event;
         if (target.files) setFile(target.files[0]);
     }
-
+    console.log(file)
     return (
-        <div
-            onClick={onDropAreaClick}
-            onDrop={onDrop}
-            onDragOver={onDropOver}
-            onDragLeave={onDropLeave}
-            className={over ? "upload-container over" : "upload-container"}
-        >
-            <h2>Upload files here!</h2>
-            <input
-                style={{ display: "none" }}
-                type="file"
-                ref={inputRef}
-                onChange={onInputChange}
-            />
+        <div className="modal">
+            <div className="modal__topbar">
+                <button className="close-button" onClick={() => setModalVisible(false)}>&#10006;</button>
+            </div>
+            <div className="divider" />
+            <div className="upload-container__wrapper">
+                <div
+                    className={`upload-container ${over ? "over" : ""}`}
+                >
+                    <img className="upload-container__image" src={`${over ? rainSVG : cloudsSVG}`} alt="" />
+                    <p className='title'>Drop your file here!</p>
+                    <p className='sub-title text--grey'>Note, that only 1 at a time is allowed</p>
+                    <p className='sub-title text--grey'>
+                        and with extensions <span className='text text--blue'>.txt</span> or <span className='text text--blue'>.doc</span>
+                    </p>
+                    <input
+                        style={{ display: "none" }}
+                        type="file"
+                        ref={inputRef}
+                        onChange={onInputChange}
+                    />
+                    <div
+                        onClick={onDropAreaClick}
+                        onDrop={onDrop}
+                        onDragOver={onDropOver}
+                        onDragLeave={onDropLeave}
+                        className="upload-container__mask"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
